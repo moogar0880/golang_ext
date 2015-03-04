40170e0b9da345f7548e22f1731590219add88f1
moogar0880 <moogar0880@gmail.com>
2015-03-03 22:09:20 -0500

    Adding GoMap.Py_popitem

--- golang_ext/gotypes/maps.go
+++ golang_ext/gotypes/maps.go
@@ -183,7 +183,36 @@ func (self *GoMap) Py_pop(args *py.Tuple, kw *py.Dict) (py.Object, error) {
 	}
 }
 
-// Returns the map inside of a GoMap instance for easier use within Go Code
+// Provide a clear method implementation which pops and returns a specified
+// key, value pair as a 2-tuple. Will raise a KeyError if the GoMap instance
+// is empty
+func (self *GoMap) Py_popitem(args *py.Tuple, kw *py.Dict) (py.Object, error) {
+	if len(self.m) == 0 {
+		py.raise(py.KeyError)
+		return nil, nil
+	} else {
+		var key, value py.Object
+		kwlist := []string{"key", "value"}
+
+		err := ParseTupleAndKeywords(args, kw, "O", kwlist, &key, &value)
+		if val, ok := self.m[key]; ok {
+			delete(self.m, key)
+			new_tuple, err := py.PackTuple(key, val)
+			if err != nil {
+				py.None.Incref()
+				return py.None, err
+			}
+			return new_tuple, nil
+		}
+	}
+	// If m is not empty, but doesn't contain the key, value pair provided,
+	// Raise a KeyError and return (py.None, nil)
+	py.raise(py.KeyError)
+	py.None.Incref()
+	return py.None, nil
+}
+
+// Returns the map inside of a GoMap instance for ease of use within Go Code
 func (self *GoMap) GoMap() map[py.Object]py.Object {
 	return self.m
 }
@@ -191,14 +220,13 @@ func (self *GoMap) GoMap() map[py.Object]py.Object {
 // TODO fromkeys :: Returns a new dict with keys from iterable and values equal to value.
 // TODO items :: D.items() -> a set-like object providing a view on D's items
 // TODO keys :: D.keys() -> a set-like object providing a view on D's keys
-// TODO popitem :: D.popitem() -> (k, v), remove and return some (key, value) pair as a 2-tuple; but raise KeyError if D is empty.
 // TODO setdefault :: D.setdefault(k[,d]) -> D.get(k,d), also set D[k]=d if k not in D
 // TODO update :: D.update([E, ]**F) -> None.  Update D from dict/iterable E and F. If E is present and has a .keys() method, then does:  for k in E: D[k] = E[k] If E is present and lacks a .keys() method, then does:  for k, v in E: D[k] = v In either case, this is followed by: for k in F:  D[k] = F[k]
 // TODO values :: D.values() -> an object providing a view on D's values
 
 // Docstring variables
 var (
-	GoMapDoc = "A native Go Maping type that implements the same interface as a Python dict."
+	GoMapDoc = "A native Go Mapping type that implements the same interface as a Python dict."
 )
 
 // Classtype Variables
